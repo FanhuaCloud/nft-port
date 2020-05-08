8cd3305cea59520192f200409423adeb90604091
FanHuaCloud <hekaihao2015@gmail.com>
2020-05-08 11:43:56 +0800

    1.2 release

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
 		logger.Error(err)
 	}
 	//输出版本信息
-	logger.Info("nft-port version 1.1")
+	logger.Info("nft-port version 1.2")
 	logger.Info("Aauthor: https://github.com/FanhuaCloud")
 
 	//设置flag
